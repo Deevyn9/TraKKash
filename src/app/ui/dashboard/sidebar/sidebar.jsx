e9c6b9368d3c81f97f9c8a2fd8c3516004ed9e02
Divine Obeten <divineobeten9@gmail.com>
2024-01-04 05:12:58 +0100

    add sidebar links

--- src/app/ui/dashboard/sidebar/sidebar.jsx
+++ src/app/ui/dashboard/sidebar/sidebar.jsx
@@ -1,6 +1,20 @@
+import Link from "next/link"
+
 const Sidebar = () => {
     return (
-        <div className="w-52 border-2 border-dashed border-yellow-500 h-screen">Sidebar</div>
+        <div className="w-52 border-2 border-dashed border-yellow-500 h-screen">
+            <ul>
+            <Link href="/dashboard">
+                    <li>main dash</li>
+                </Link>
+                <Link href="/dashboard/income">
+                    <li>income</li>
+                </Link>
+                <Link href="/dashboard/expenses">
+                    <li>expenses</li>
+                </Link>
+            </ul>
+        </div>
     )
 }
 
