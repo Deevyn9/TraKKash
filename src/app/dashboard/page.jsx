c1f8ea92fbc4d0d036857ce4b138d83fb9f91393
Divine Obeten <divineobeten9@gmail.com>
2024-01-04 16:17:54 +0100

    add balance summary

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,6 +1,15 @@
 const Dashboard = () => {
     return (
-        <div>Dashboard</div>
+        <div className="p-5">
+            <div >
+                <h3 className="text-2xl">My Balance</h3>
+                <p className="text-7xl font-bold mb-5">$2,000</p>
+                <div>
+                    <button className="bg-green-500 w-28 h-10 text-white rounded-lg cursor-pointer mr-5">Add Income</button>
+                    <button className="bg-red-500 w-28 h-10 text-white rounded-lg cursor-pointer">Add Expense</button>
+                </div>
+            </div>
+        </div>
     )
 }
 
