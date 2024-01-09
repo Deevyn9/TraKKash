6fb931a7476548418851999d21441e9b18cf44ee
Divine Obeten <divineobeten9@gmail.com>
2024-01-09 20:17:36 +0100

    connecting expense component to firestore

--- src/app/dashboard/expenses/page.jsx
+++ src/app/dashboard/expenses/page.jsx
@@ -1,7 +1,43 @@
-const ExpensesPage = () => {
-    return (
-        <div>ExpensesPage</div>
-    )
-}
+"use client";
+import { useState, useEffect, use } from "react";
+import { currencyFormatter } from "../../../../lib/utils";
+import { db } from "../../../../firebase";
+import { collection, addDoc, getDocs } from "firebase/firestore";
 
-export default ExpensesPage
+const ExpensesPage = () => {
+  const [expense, setExpense] = useState([]);
+
+  useEffect(() => {
+    const getExpenseData = async () => {
+      const collectionRef = collection(db, "expense");
+      const docsSnap = await getDocs(collectionRef);
+
+      const data = docsSnap.docs.map((doc) => {
+        return {
+          id: doc.id,
+          ...doc.data(),
+          createdAt: new Date(doc.data().createdAt.toMillis()),
+        };
+      });
+      setExpense(data);
+    };
+
+    getExpenseData();
+  }, []);
+
+  return (
+    <div>
+      {expense.map((i) => {
+        return (
+          <div key={i.id}>
+            <div>{i.description}</div>
+            <small>{i.createdAt.toISOString()}</small>
+            <p>{currencyFormatter(i.amount)}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default ExpensesPage;
