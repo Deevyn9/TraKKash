6dec725691abbfdf0286b0bb0c1a948def61160f
Divine Obeten <divineobeten9@gmail.com>
2024-01-09 20:05:52 +0100

    connecting income component to firestore

--- src/app/dashboard/income/page.jsx
+++ src/app/dashboard/income/page.jsx
@@ -1,7 +1,44 @@
-const IncomePage = () => {
-    return (
-        <div>IncomePage</div>
-    )
-}
+"use client";
+import { useState, useEffect } from "react";
+import { currencyFormatter } from "../../../../lib/utils";
+import { db } from "../../../../firebase";
+import { collection, addDoc, getDocs } from "firebase/firestore";
 
-export default IncomePage
+const IncomePage = () => {
+  const [income, setIncome] = useState([]);
+  //   console.log(income);
+
+  useEffect(() => {
+    const getIncomeData = async () => {
+      const collectionRef = collection(db, "income");
+      const docsSnap = await getDocs(collectionRef);
+
+      const data = docsSnap.docs.map((doc) => {
+        return {
+          id: doc.id,
+          ...doc.data(),
+          createdAt: new Date(doc.data().createdAt.toMillis()),
+        };
+      });
+      setIncome(data);
+    };
+
+    getIncomeData();
+  }, []);
+
+  return (
+    <div>
+      {income.map((i) => {
+        return (
+          <div key={i.id}>
+            <div className="font-semi-bold">{i.description}</div>
+            <small>{i.createdAt.toISOString()}</small>
+            <p>{currencyFormatter(i.amount)}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default IncomePage;
