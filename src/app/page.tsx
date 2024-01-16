a6187386dfa405b6a19fd515d3a484383635e666
Divine Obeten <divineobeten9@gmail.com>
2024-01-16 12:16:44 +0100

    make main page responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,27 +11,30 @@ export default function Home() {
   }
 
   return (
-    <main className="flex flex-col justify-center items-center w-screen h-screen">
+    <main className="relative flex flex-col justify-center items-center w-screen h-screen overflow-hidden">
+      <div className="sun absolute w-screen bg-gradient-to-b from-purple-500 to-purple-300 blur-3xl opacity-75 h-4/6 -z-10 border-r inset-y-16 inset-x-0"></div>
+
       <div className="mb-8">
         <h1 className="font-black text-5xl text-center sm:text-9xl">
-          traKKash<span className="text-green-500">.</span>
+          traKKash<span className="text-purple-500">.</span>
         </h1>
         <p className="text-lg sm:text-2xl text-center">
-          Know where your money goes<span className="text-green-500">!</span>
+          Know where your money goes{" "}
+          {/*<span className="text-purple-500">!</span> */}
         </p>
       </div>
 
       <div className="flex gap-5">
         <Link
           href="/sign-in"
-          className="sm:border-8 border-solid border-green-500 bg-green-500 rounded-md w-28 h-10 sm:py-2 sm:px-10 transition-all hover:bg-transparent text-lg"
+          className="home-btn sm:border-8 border-solid border-purple-500 bg-purple-500 rounded-md sm:py-2 sm:px-10 transition-all hover:bg-transparent text-lg"
         >
           <button className="w-full h-full">Sign In</button>
         </Link>
 
         <Link
           href="/sign-up"
-          className="sm:border-8 border-2 border-solid border-green-500 rounded-md w-28 h-10 sm:py-2 sm:px-10 transition-all hover:bg-green-500 text-lg"
+          className="home-btn sm:border-8 border-2 border-solid border-purple-500 rounded-md sm:py-2 sm:px-10 transition-all hover:bg-purple-500 text-lg"
         >
           <button className="w-full h-full">Sign Up</button>
         </Link>
