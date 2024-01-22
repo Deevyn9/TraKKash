ba59e8d4f705c3e8c498aed349b7b8fd30a036f4
Divine Obeten <divineobeten9@gmail.com>
2024-01-22 21:28:55 +0100

    add pathname styles

--- src/app/ui/dashboard/sidebar/sidebar.jsx
+++ src/app/ui/dashboard/sidebar/sidebar.jsx
@@ -1,18 +1,36 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const Sidebar = () => {
+  const pathname = usePathname();
+
   return (
     <div className="md:nav-side__container hidden w-1/5 lg:w-2/12 h-full">
       <ul className="nav-side lg:px-5">
         <Link href="/dashboard" className="sidebar-links">
-          <li className="sidebar-links__main hover:text-purple-500">
+          <li
+            className={`sidebar-links__main ${
+              pathname === "/dashboard" ? "bg-red-500" : ""
+            } hover:text-purple-500`}
+          >
             main dash
           </li>
         </Link>
-        <Link href="/dashboard/income" className="sidebar-links">
+        <Link
+          href="/dashboard/income"
+          className={`sidebar-links__main ${
+            pathname === "/dashboard/income" ? "bg-red-500" : ""
+          } hover:text-purple-500`}
+        >
           <li className="sidebar-links__main">income</li>
         </Link>
-        <Link href="/dashboard/expenses" className="sidebar-links">
+        <Link
+          href="/dashboard/expenses"
+          className={`sidebar-links__main ${
+            pathname === "/dashboard/expenses" ? "bg-red-500" : ""
+          } hover:text-purple-500`}
+        >
           <li className="sidebar-links__main">expenses</li>
         </Link>
       </ul>
