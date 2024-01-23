264567bc60eb350edccb63928e642326a6153c03
Divine Obeten <divineobeten9@gmail.com>
2024-01-23 21:04:12 +0100

    conditionally render input and output logs

--- src/app/dashboard/expenses/page.jsx
+++ src/app/dashboard/expenses/page.jsx
@@ -67,7 +67,7 @@ const ExpensesPage = () => {
   };
 
   return (
-    <div className="nav-side__container md:w-4/5 w-screen lg:w-5/6x ">
+    <div className="nav-side__container md:w-4/5 w-screen lg:w-5/6">
       <div className="overflow-scroll nav-side flex flex-col py-4">
         <Link href="/dashboard">
           <p className="flex items-center mb-5 md:hidden">
@@ -78,7 +78,7 @@ const ExpensesPage = () => {
               viewBox="0 0 24 24"
               strokeWidth={1.5}
               stroke="currentColor"
-              className="w-6 h-6 ml-2"
+              className="w-6 h-6 ml-1"
             >
               <path
                 strokeLinecap="round"
@@ -89,45 +89,53 @@ const ExpensesPage = () => {
           </p>
         </Link>
 
-        {expense.map((i) => {
-          return (
-            <div
-              key={i.id}
-              className="flex justify-between items-center border-2 border-solid border-purple-600 h-24 p-4 rounded-lg mb-5"
-            >
-              <div className="h-full flex flex-col justify-between">
-                <div className="font-semi-bold capitalize sm:text-xl">
-                  {i.description}
+        {expense.length > 0 ? (
+          expense.map((i) => {
+            return (
+              <div
+                key={i.id}
+                className="flex justify-between items-center border-2 border-solid border-purple-600 h-24 p-4 rounded-lg mb-5"
+              >
+                <div className="h-full flex flex-col justify-between">
+                  <div className="font-semi-bold capitalize sm:text-xl">
+                    {i.description}
+                  </div>
+                  <small>{i.createdAt.toLocaleDateString()}</small>
                 </div>
-                <small>{i.createdAt.toLocaleDateString()}</small>
-              </div>
-              <div className="flex h-full flex-col sm:flex-row sm:items-center items-end justify-between">
-                <div className="sm:w-max overflow-hidden">
-                  <p className="sm:mr-3 sm:text-lg  overflow-x-scroll">
-                    {currencyFormatter(i.amount)}
-                  </p>
-                </div>
+                <div className="flex h-full flex-col sm:flex-row sm:items-center items-end justify-between">
+                  <div className="sm:w-max overflow-hidden">
+                    <p className="sm:mr-3 sm:text-lg  overflow-x-scroll">
+                      {currencyFormatter(i.amount)}
+                    </p>
+                  </div>
 
-                <button onClick={() => deleteLog(i.id)}>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="currentColor"
-                    className="w-6 h-6"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
-                    />
-                  </svg>
-                </button>
+                  <button onClick={() => deleteLog(i.id)}>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="w-6 h-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                      />
+                    </svg>
+                  </button>
+                </div>
               </div>
-            </div>
-          );
-        })}
+            );
+          })
+        ) : (
+          <div className="text-2xl capitalize place-self-center mt-28 md:mt-0 self-center">
+            nothing to show here
+          </div>
+        )}
+
+        {}
       </div>
     </div>
   );
--- src/app/dashboard/income/page.jsx
+++ src/app/dashboard/income/page.jsx
@@ -87,46 +87,54 @@ const IncomePage = () => {
           </p>
         </Link>
 
-        {income.map((i) => {
-          return (
-            <div
-              key={i.id}
-              className="flex justify-between items-center border-2 border-solid border-purple-600 h-24 p-4 rounded-lg mb-5  "
-            >
-              <div className="h-full flex flex-col justify-between w-2/4 overflow-hidden pr-2">
-                <div className="font-semi-bold capitalize sm:text-xl overflow-x-scroll">
-                  {i.description}
-                </div>
-                <small>{i.createdAt.toLocaleDateString()}</small>
-              </div>
-
-              <div className="flex h-full flex-col sm:flex-row sm:items-center items-end justify-between sm:justify-en w-2/4 overflow-hidden pl-2">
-                <div className="overflow-x-scroll w-full text-right ">
-                  <p className="sm:mr-3 sm:text-lg">
-                    {currencyFormatter(i.amount)}
-                  </p>
+        {income.length > 0 ? (
+          income.map((i) => {
+            return (
+              <div
+                key={i.id}
+                className="flex justify-between items-center border-2 border-solid border-purple-600 h-24 p-4 rounded-lg mb-5  "
+              >
+                <div className="h-full flex flex-col justify-between w-2/4 overflow-hidden pr-2">
+                  <div className="font-semi-bold capitalize sm:text-xl overflow-x-scroll">
+                    {i.description}
+                  </div>
+                  <small>{i.createdAt.toLocaleDateString()}</small>
                 </div>
 
-                <button onClick={() => deleteLog(i.id)}>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="currentColor"
-                    className="w-6 h-6"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
-                    />
-                  </svg>
-                </button>
+                <div className="flex h-full flex-col sm:flex-row sm:items-center items-end justify-between sm:justify-en w-2/4 overflow-hidden pl-2">
+                  <div className="overflow-x-scroll w-full text-right ">
+                    <p className="sm:mr-3 sm:text-lg">
+                      {currencyFormatter(i.amount)}
+                    </p>
+                  </div>
+
+                  <button onClick={() => deleteLog(i.id)}>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="w-6 h-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                      />
+                    </svg>
+                  </button>
+                </div>
               </div>
-            </div>
-          );
-        })}
+            );
+          })
+        ) : (
+          <div className="text-2xl capitalize place-self-center mt-28 md:mt-0 self-center">
+            nothing to show here
+          </div>
+        )}
+
+        {}
       </div>
     </div>
   );
