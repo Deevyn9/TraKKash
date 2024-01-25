81f4cb56c804e831746e614bfa03852ecb591da2
Divine Obeten <divineobeten9@gmail.com>
2024-01-25 20:35:51 +0100

    toggle sidebar link background

--- src/app/ui/dashboard/sidebar/sidebar.jsx
+++ src/app/ui/dashboard/sidebar/sidebar.jsx
@@ -6,32 +6,40 @@ const Sidebar = () => {
   const pathname = usePathname();
 
   return (
-    <div className="md:nav-side__container hidden w-1/5 lg:w-2/12 h-full">
-      <ul className="nav-side lg:px-5">
-        <Link href="/dashboard" className="sidebar-links">
+    <div className="md:nav-side__container hidden w-1/5 lg:w-2/12 h-full border-r-2 border-black border-solid border-0">
+      <ul className="nav-side">
+        <Link href="/dashboard" className={`sidebar-links__main`}>
           <li
             className={`sidebar-links__main ${
-              pathname === "/dashboard" ? "bg-red-500" : ""
-            } hover:text-purple-500`}
+              pathname === "/dashboard"
+                ? "bg-purple-500 text-white"
+                : "hover:text-purple-500"
+            } h-full w-full flex items-center px-2 capitalize`}
           >
-            main dash
+            main dashboard
           </li>
         </Link>
-        <Link
-          href="/dashboard/income"
-          className={`sidebar-links__main ${
-            pathname === "/dashboard/income" ? "bg-red-500" : ""
-          } hover:text-purple-500`}
-        >
-          <li className="sidebar-links__main">income</li>
+        <Link href="/dashboard/income" className={`sidebar-links__main`}>
+          <li
+            className={`sidebar-links__main ${
+              pathname === "/dashboard/income"
+                ? "bg-purple-500 text-white"
+                : "hover:text-purple-500"
+            } h-full w-full flex items-center px-2 capitalize`}
+          >
+            income
+          </li>
         </Link>
-        <Link
-          href="/dashboard/expenses"
-          className={`sidebar-links__main ${
-            pathname === "/dashboard/expenses" ? "bg-red-500" : ""
-          } hover:text-purple-500`}
-        >
-          <li className="sidebar-links__main">expenses</li>
+        <Link href="/dashboard/expenses" className={`sidebar-links__main`}>
+          <li
+            className={`sidebar-links__main ${
+              pathname === "/dashboard/expenses"
+                ? "bg-purple-500 text-white"
+                : "hover:text-purple-500"
+            } h-full w-full flex items-center px-2 capitalize`}
+          >
+            expenses
+          </li>
         </Link>
       </ul>
     </div>
