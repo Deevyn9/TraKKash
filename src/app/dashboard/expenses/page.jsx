494fee9c601a39e6c279a2ee7edd2d4c09b0e5be
Divine Obeten <divineobeten9@gmail.com>
2024-02-05 20:55:07 +0100

    remove delete function

--- src/app/dashboard/expenses/page.jsx
+++ src/app/dashboard/expenses/page.jsx
@@ -67,29 +67,29 @@ const ExpensePage = () => {
     loadDataFromLocalStorage();
   }, [user, logsCollectionRef, setLogsCollectionRef]);
 
-  const deleteLog = async (logId) => {
-    try {
-      if (!logsCollectionRef) {
-        console.error("logsCollectionRef is not initialized");
-        return;
-      }
-      const logRef = doc(logsCollectionRef, logId);
-      await deleteDoc(logRef);
-      console.log(`Log with ID ${logId} deleted successfully`);
-      setExpense((prevExpense) =>
-        prevExpense.filter((log) => log.id !== logId)
-      );
+  // const deleteLog = async (logId) => {
+  //   try {
+  //     if (!logsCollectionRef) {
+  //       console.error("logsCollectionRef is not initialized");
+  //       return;
+  //     }
+  //     const logRef = doc(logsCollectionRef, logId);
+  //     await deleteDoc(logRef);
+  //     console.log(`Log with ID ${logId} deleted successfully`);
+  //     setExpense((prevExpense) =>
+  //       prevExpense.filter((log) => log.id !== logId)
+  //     );
 
-      const updatedExpense = expense.filter((log) => log.id !== logId);
-      localStorage.setItem("expense", JSON.stringify(updatedExpense));
-    } catch (error) {
-      console.error("Error deleting log:", error);
-    }
-  };
+  //     const updatedExpense = expense.filter((log) => log.id !== logId);
+  //     localStorage.setItem("expense", JSON.stringify(updatedExpense));
+  //   } catch (error) {
+  //     console.error("Error deleting log:", error);
+  //   }
+  // };
 
   return (
     <div className="nav-side__container md:w-4/5 w-screen lg:w-5/6 overflow-y-scroll pb-5">
-      <div className="nav-side flex flex-col p-4">
+      <div className="nav-side flex flex-col p-4 ">
         <Link href="/dashboard">
           <p className="flex items-center mb-5 md:hidden">
             Back to Maindash
@@ -110,62 +110,64 @@ const ExpensePage = () => {
           </p>
         </Link>
 
-        {expense.length > 0 ? (
-          expense.map((i) => {
-            return (
-              <div
-                key={i.id}
-                className="flex justify-between items-center border-2 border-solid border-purple-600 h-24 p-4 rounded-lg mb-5  md:hover:bg-purple-300 transition-all"
-              >
-                <div className="h-full flex flex-col justify-between pr-2">
-                  <div className="font-semi-bold capitalize sm:text-xl ">
-                    {i.description}
-                  </div>
-                  <small>
-                    {i.createdAt instanceof Date
-                      ? i.createdAt.toLocaleDateString()
-                      : "Loading..."}
-                  </small>
-                </div>
-
-                <div className="flex h-full flex-col sm:flex-row sm:items-center items-end justify-between sm:justify-en w-2/4 pl-2">
-                  <div className="w-full text-right ">
-                    <p className="sm:mr-3 text-red-600 sm:text-lg">
-                      {currencyFormatter(i.amount)}
-                    </p>
+        <div className="logs-container">
+          {expense.length > 0 ? (
+            expense.map((i) => {
+              return (
+                <div
+                  key={i.id}
+                  className="flex justify-between items-center border-2 border-solid border-purple-600 h-24 p-4 rounded-lg mb-5  md:hover:bg-purple-300 transition-all"
+                >
+                  <div className="h-full flex flex-col justify-between pr-2">
+                    <div className="font-semi-bold capitalize sm:text-xl ">
+                      {i.description}
+                    </div>
+                    <small>
+                      {i.createdAt instanceof Date
+                        ? i.createdAt.toLocaleDateString()
+                        : "Loading..."}
+                    </small>
                   </div>
 
-                  <button onClick={() => deleteLog(i.id)}>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth={1.5}
-                      stroke="currentColor"
-                      className="w-6 h-6"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
-                      />
-                    </svg>
-                  </button>
+                  <div className="flex h-full flex-col sm:flex-row sm:items-center items-end justify-between sm:justify-en w-2/4 pl-2">
+                    <div className="w-full text-right ">
+                      <p className="sm:mr-3 text-red-600 sm:text-lg">
+                        {currencyFormatter(i.amount)}
+                      </p>
+                    </div>
+
+                    {/* <button onClick={() => deleteLog(i.id)}>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="w-6 h-6"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                        />
+                      </svg>
+                    </button> */}
+                  </div>
                 </div>
-              </div>
-            );
-          })
-        ) : (
-          <div className="capitalize place-self-center mt-10 self-center">
-            <Image src={expenseIllustration} alt="no expenses" />
-            <p
-              className="text-2xl text-center mt-5098765432
+              );
+            })
+          ) : (
+            <div className="capitalize place-self-center mt-10 self-center">
+              <Image src={expenseIllustration} alt="no expenses" />
+              <p
+                className="text-2xl text-center mt-5098765432
             "
-            >
-              start spending!
-            </p>
-          </div>
-        )}
+              >
+                start spending!
+              </p>
+            </div>
+          )}
+        </div>
 
         {}
       </div>
